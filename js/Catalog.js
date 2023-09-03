a7eaaec9cd9ac6cfe321db53bb6e7895a8c4c156
Daritork <87564532+Daritork@users.noreply.github.com>
2023-09-03 20:45:53 +0200

    Added Catalog, easy to adjust

--- js/Catalog.js
+++ js/Catalog.js
@@ -1,19 +1,42 @@
-let items = ["Green Tea", "Bag", "Accesories", "Presents", "Box"];
-let itemPrice = [5.00, 5.00, 5.00, 5.00, 5.00];
+var itemInRow = 4;
+var rowNum = 0;
+let items = [["Green Tea", 5.05], ["Bag", 6.2], ["Accesories", 4.95], ["Presents", 7.99], ["Box", 3.95]];
+for (let i = 0; i < items.length; i++) {
+    let rowNumId = "Row" + (rowNum - 1);
+    let itemProperties = items[i];
+    const list = document.getElementById("itemList");
+    const rowWithElements = document.getElementById(rowNumId);
+    const newElement = document.createElement('td');
+    const itemImage = document.createElement('img');
+    itemImage.setAttribute("src", "sitepic/catalog/GreenTea.png");
+    const row1 = document.createElement('div');
+    const itemName = document.createElement('h2');
+    const itemNameNode = document.createTextNode(itemProperties[0]);
+    const addToFavButton = document.createElement('button');
+    addToFavButton.setAttribute("class", "favBut");
+    const addToFavButontFavicon = document.createElement("img");
+    addToFavButontFavicon.setAttribute("src", "sitepic/catalog/favourite.png", "class", "favButImg");
+    const row2 = document.createElement('div');
+    const itemPrice = document.createElement('p');
+    let textNode = document.createTextNode(itemProperties[1] + " €");
+    const buyButton = document.createElement('button');
+    buyButton.setAttribute("class", "buyBut");
+    const buyButtonText = document.createTextNode("Buy");
+    /*Appending*/
+    buyButton.appendChild(buyButtonText);
+    itemPrice.appendChild(textNode);
+    row2.appendChild(itemPrice);
+    row2.appendChild(buyButton);
+    addToFavButton.appendChild(addToFavButontFavicon);
+    itemName.appendChild(itemNameNode);
+    row1.appendChild(itemName);
+    row1.appendChild(addToFavButton);
+    newElement.appendChild(itemImage);
+    newElement.appendChild(row1);
+    newElement.appendChild(row2);
+    list.appendChild(newElement);
+    itemInRow++;
+}
 function Filter() {
-    for (let i = 0; i < items.length; i++) {
-        const listBody = document.getElementById("itemList");
-        const newElement = document.createElement('td');
-        const itemImage = document.createElement('img');
-        const row1 = document.createElement('div');
-        const row2 = document.createElement('div');
-        const itemName = document.createElement('p');
-        const addToCartButton = document.createElement('button');
-        const itemPrice = document.createElement('p');
-        const buyButton = document.createElement('button');
-        const textNode = document.createTextNode(items[i]);
-        itemName.appendChild(textNode);
-        newElement.appendChild(itemName);
-        listBody.appendChild(newElement);
-    }
+    
 }
